797a65d7fdc37a4f6bb8ac2a429de1a9a7367ed5
Eric Walsh <ericmatthewwalsh@gmail.com>
2014-12-07 12:53:24 -0800

    updates

--- testGet.py
+++ testGet.py
@@ -5,10 +5,11 @@ startTime = datetime.now()
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2107.3 Safari/537.36', 'Host': 'secure-store.nike.com', 'Connection': 'keep-alive', 'Referer': 'link', 'Accept-Encoding': 'gzip,deflate', 'Accept-Language': 'en-US,en;q=0.8'} 
 cookie = {'RES_TRACKINGID': '849445096449926', 'RES_SESSIONID': '35166464048501571', 'AnalysisUserId': '128.999.77.22.1405689197999243', 'WRUID': '999997038.176257895', 'AKNIKE': 'aaaU7Y_RwjGvd9fnDJr9XSPidl715aSZs0uylTgXzcHxT2VFBj4hVOw', 'USID': '0EA26227358182A1AEACC9C39CABDEAF.sin-197-app-us-1', 'guidS': '030ca47a-4f90-4894-a8ad-70f7f4a96550', 'guidU': '5b2f32fc-91b7-4e8d-e2e1-1e733e171917', 'utag_main': '_st:1406883408970$ses_id:1406881306602%3Bexp-session', 'needlepin': 'N190d1405448840085428df001c216777a328777900add77900fd1001900000000000001177900b9e0000000009usrunning01177900bc72ea0000', '_unam': 'c0158ee-1474310f9da-28ebb10-1'}
-payload = ###
+payload = 
+sesh = requests.Session()
 #r = requests.session().post(link,headers=headers,cookies=cookie)
 
-r = requests.get('http://store.nike.com/us/en_us/pd/free-4-flyknit-running-shoe/pid-1545960/pgid-1481072')
+r = sesh.get('http://store.nike.com/us/en_us/pd/free-4-flyknit-running-shoe/pid-1545960/pgid-1481072')
 
 
 
@@ -26,12 +27,12 @@ link = "https://secure-store.nike.com/us/services/jcartService?callback=nike_Car
 link =link.replace(' ','+')
 link = link.replace('&#x27;', '%27')
 #print link
-s = requests.post("https://www.nike.com/profile/login?Content-Locale=en_US", headers=headers, params=payload)
+s = sesh.post("https://www.nike.com/profile/login?Content-Locale=en_US", params=payload)
 print s.content
 print "XXXXXXXXXXXXXXXXXXXX"
 print " "
 
-t = requests.post(link, headers=headers, cookies=cookie)
+t = sesh.post(link, headers=headers, cookies=cookie)
 print t.content
 
 text_file1 = open("nikeresp.txt", "w")
